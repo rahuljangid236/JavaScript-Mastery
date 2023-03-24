83fc16aebeaec40a6e2476bbb81f8b194734e94f
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-25 01:45:15 +0530

    Update javascriptBasic.js

--- javascriptBasic.js
+++ javascriptBasic.js
@@ -6,5 +6,28 @@ console.log(firstName);
 var firstName = "Kamlesh";
 console.log(firstName);
 let lastName = "Jangid";
-console.log(lastName.length);
-console.log(lastName[0]);
+// console.log(lastName.length);
+// console.log(lastName[0]);
+// console.log(lastName[lastName.length - 1]);
+
+// String Method
+// let lastNameTrim = lastName.trim();
+// console.log(lastNameTrim.length);
+// let toUpperCase = lastName.toUpperCase();
+// let toLowerCase = lastName.toLowerCase();
+// console.log(toUpperCase);
+// console.log(toLowerCase);
+
+// Slice
+let SlicelastName = lastName.slice(0, 3);
+console.log(SlicelastName);
+
+// type of Operator
+let age = 22;
+let name = "Rahul";
+console.log(typeof age);
+console.log(typeof name);
+let num = age + "";
+console.log(typeof num);
+let strToNum = +"Hello";
+console.log(typeof strToNum);
