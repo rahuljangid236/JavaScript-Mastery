0dc5ffd62b40ead117aa4faf931059ac8919a1dd
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-26 22:42:46 +0530

    Update javascriptBasic.js

--- javascriptBasic.js
+++ javascriptBasic.js
@@ -1,11 +1,11 @@
 "use strict";
 // Variables
 
-var firstName = "Rahul";
-console.log(firstName);
-var firstName = "Kamlesh";
-console.log(firstName);
-let lastName = "Jangid";
+// var firstName = "Rahul";
+// console.log(firstName);
+// var firstName = "Kamlesh";
+// console.log(firstName);
+// let lastName = "Jangid";
 // console.log(lastName.length);
 // console.log(lastName[0]);
 // console.log(lastName[lastName.length - 1]);
@@ -19,15 +19,32 @@ let lastName = "Jangid";
 // console.log(toLowerCase);
 
 // Slice
-let SlicelastName = lastName.slice(0, 3);
-console.log(SlicelastName);
+// let SlicelastName = lastName.slice(0, 3);
+// console.log(SlicelastName);
 
-// type of Operator
-let age = 22;
+// // type of Operator
+// let age = 22;
+// let name = "Rahul";
+// console.log(typeof age);
+// console.log(typeof name);
+// let num = age + "";
+// console.log(typeof num);
+// let strToNum = +"Hello";
+// console.log(typeof strToNum);
+
+// let age = 22;
+// age = String(age);
+// console.log(typeof age);
+// age = Number(age);
+// console.log(typeof age);
+
+// Template String
 let name = "Rahul";
-console.log(typeof age);
-console.log(typeof name);
-let num = age + "";
-console.log(typeof num);
-let strToNum = +"Hello";
-console.log(typeof strToNum);
+let age = 23;
+let about = `My name is ${name} and my age is ${age}.`;
+
+console.log(about);
+console.log(typeof null);
+console.log(Number.MAX_SAFE_INTEGER);
+let bigInt = BigInt(123154646544615646516156161);
+console.log(typeof bigInt);
