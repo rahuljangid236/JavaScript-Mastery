de3a9e1d0348d7bf84c7c3586c4b45146b812fed
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-28 01:38:36 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -10,3 +10,27 @@
 // for (let i = 0; i <= 9; i++) {
 //   console.log(i);
 // }
+
+// Break Keyword
+// for (let i = 0; i <= 9; i++) {
+//   if (i === 4) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// Continue Keyword
+// for (let i = 0; i <= 9; i++) {
+//   if (i === 4) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// do While Loop
+
+let i = 10;
+do {
+  console.log(i);
+  i++;
+} while (i <= 9);
