2d096cbcc9244ae4a7546f061dd642d0beb1eddf
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-28 01:33:06 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,5 +1,12 @@
-let i = 0;
-while (i <= 9) {
-  console.log(i);
-  i++;
-}
+// While Loop
+
+// let i = 0;
+// while (i <= 9) {
+//   console.log(i);
+//   i++;
+// }
+
+// For Loop
+// for (let i = 0; i <= 9; i++) {
+//   console.log(i);
+// }
