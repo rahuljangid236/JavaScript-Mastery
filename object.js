fe8001d94cf80cb6e697bae164fbb04523f67315
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-29 01:33:57 +0530

    Update object.js

---
+++ object.js
@@ -0,0 +1,11 @@
+// objects are refernce type
+// arrays are good but not sufficient
+// for real world data
+// object stores key value pairs
+
+// how to create objects
+const person = {
+  name: "Rahul",
+  age: 23,
+};
+console.log(person);
