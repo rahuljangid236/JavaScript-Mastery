32a582caa7ba8386748abd8fa5ddcbc94e1754b6
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-31 17:35:55 +0530

    Update object.js

--- object.js
+++ object.js
@@ -4,11 +4,11 @@
 // object stores key value pairs
 
 // how to create objects
-const person = {
-  name: "Rahul",
-  age: 23,
-  hobbies: ["guitar", "sleeping", "listening music"],
-};
+// const person = {
+//   name: "Rahul",
+//   age: 23,
+//   hobbies: ["guitar", "sleeping", "listening music"],
+// };
 
 // how to access data from object
 // console.log(person.name);
@@ -36,22 +36,43 @@ const person = {
 
 // Computed Properties
 
-const key1 = "objkey1";
-const key2 = "objkey2";
+// const key1 = "objkey1";
+// const key2 = "objkey2";
 
-const obj1 = {
-  [key1]: "value1",
-  [key2]: "value2",
-};
+// const obj1 = {
+//   [key1]: "value1",
+//   [key2]: "value2",
+// };
 // console.log(obj1);
 
 // Spread Operator in obj
 
-const obj2 = {
-  key1: "unique",
-  key3: "value3",
-  key4: "value4",
-};
+// const obj2 = {
+//   key1: "unique",
+//   key3: "value3",
+//   key4: "value4",
+// };
 
-const newObject = { ...obj1, ...obj2 };
-console.log(newObject);
+// const newObject = { ...obj1, ...obj2 };
+// console.log(newObject);
+
+// Object destructuring
+
+// const { key1, key3, ...restProps } = obj2;
+// console.log(key1);
+// console.log(key3);
+// console.log(restProps);
+
+const users = [
+  { userId: 1, firstName: "Rahul", gender: "Male" },
+  { userId: 2, firstName: "Raju", gender: "Male" },
+  { userId: 3, firstName: "Rakesh", gender: "Male" },
+];
+
+for (let user of users) {
+  console.log(user);
+}
+
+const [{ firstName }, , { gender }] = users;
+console.log(firstName);
+console.log(gender);
