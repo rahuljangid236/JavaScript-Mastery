cbb91c36a5c5e866f6864792c76b1687f3004e3f
Rahul Jangid <rahuljangid236@gmail.com>
2023-04-02 00:27:39 +0530

    Update arrayMethod.js

---
+++ arrayMethod.js
@@ -0,0 +1,43 @@
+// ForEach
+// const numbers = [4, 2, 5, 8];
+
+// function myFunc(number, index) {
+//   console.log(`index is ${index} number is ${number}`);
+// }
+
+// numbers.forEach(myFunc);
+
+// const users = [
+//   { firstName: "Harshit", age: 23 },
+//   { firstName: "Subham", age: 20 },
+//   { firstName: "Rakesh", age: 21 },
+//   { firstName: "Rahul", age: 22 },
+// ];
+
+// users.forEach(function (user) {
+//   console.log(user.firstName);
+// });
+
+// map Method
+// const numbers = [3, 4, 6, 1, 8];
+// const square = function (number) {
+//   return number * number;
+// };
+// const squareNumb = numbers.map(square);
+// console.log(squareNumb);
+
+// const users = [
+//   { firstName: "Harshit", age: 23 },
+//   { firstName: "Subham", age: 20 },
+//   { firstName: "Rakesh", age: 21 },
+//   { firstName: "Rahul", age: 22 },
+// ];
+
+// const userNames = users.map((user) => {
+//   return user.firstName;
+// });
+
+// console.log(userNames);
+
+// Filter method
+const numbers = [3, 4, 6, 1, 8];
