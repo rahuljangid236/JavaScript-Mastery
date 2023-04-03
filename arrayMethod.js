17a637956414fcd499b61112e44fd476e5a146eb
Rahul Jangid <rahuljangid236@gmail.com>
2023-04-04 00:24:19 +0530

    Update arrayMethod.js

--- arrayMethod.js
+++ arrayMethod.js
@@ -52,3 +52,8 @@
 // console.log(evenNumbers);
 
 // reduce method
+const numbers = [1,2,3,4,5];
+const sum = numbers.reduce((accumulator,currentValue)=>{
+    return accumulator+currentValue;
+})
+console.log(sum);
