1764c0ef1c4bbc30eefd24912cc1e4884c97e969
Rahul Jangid <rahuljangid236@gmail.com>
2023-04-06 00:08:42 +0530

    Update object2.js

--- object2.js
+++ object2.js
@@ -7,3 +7,11 @@ const obj2 = Object.create(obj1);
 obj2.key3 = "value3";
 // console.log(obj2.key1);
 console.log(obj2.__proto__);
+
+function hello() {
+  console.log("hello world");
+}
+
+console.log(hello.name);
+hello.myOwnProperty = "value unique value";
+console.log(hello.myOwnProperty);
