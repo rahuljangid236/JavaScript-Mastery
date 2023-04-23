25c15c5f6c2ab832621cc6c20ddcb7dbcb4dcaa1
Rahul Jangid <rahuljangid236@gmail.com>
2023-04-24 01:08:46 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,8 +1,11 @@
-const demoElement = document.getElementById(demo);
-const querySel = document.querySelector(".classDemo");
-const querySelAll = document.querySelectorAll;
-// console.log(querySel);
-const link = document.querySelector("a");
-console.log(link.getAttribute("href"));
-link.setAttribute("href", "#link");
-console.log(link.getAttribute("href"));
+// const demoElement = document.getElementById(demo);
+// const querySel = document.querySelector(".classDemo");
+// const querySelAll = document.querySelectorAll;
+// // console.log(querySel);
+// const link = document.querySelector("a");
+// console.log(link.getAttribute("href"));
+// link.setAttribute("href", "#link");
+// console.log(link.getAttribute("href"));
+
+const rootNode = document.getRootNode();
+console.log(rootNode);
